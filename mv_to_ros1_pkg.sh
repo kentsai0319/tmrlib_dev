2e98ea8f567878d6619b7d1a464b57a185d2a1db
ken.tsai <ken.tsai@tm-robot.com>
2021-03-18 15:35:49 +0800

    add .sh to rename file for ros2/ros1 package.xml and CMakeLists.txt

---
+++ mv_to_ros1_pkg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mv package.xml package.ros2.xml
+mv package.ros1.xml package.xml
+mv CMakeLists.txt CMakeLists.ros2.txt
+mv CMakeLists.ros1.txt CMakeLists.txt
---
+++ mv_to_ros2_pkg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mv package.xml package.ros1.xml
+mv package.ros2.xml package.xml
+mv CMakeLists.txt CMakeLists.ros1.txt
+mv CMakeLists.ros2.txt CMakeLists.txt
